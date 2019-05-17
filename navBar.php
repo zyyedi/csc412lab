2f379b28783d5a1c724787b91872c8211c82f865
zhangyiyu <zhangedi1995@gmail.com>
2019-05-17 13:43:42 -0700

    some change

---
+++ navBar.php
@@ -0,0 +1,25 @@
+<nav id="mainNavbar" class="navbar navbar-dark navbar-expand-md py-0 fixed-top">
+    <a href="#" class="navbar-brand">DOG</a>
+    <button class="navbar-toggler" data-toggle="collapse" data-target="#navLinks" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navLinks">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a href="https://zyyedi.github.io/csc412lab/index.php" class="nav-link">HOME</a>
+            </li>
+            <li class="nav-item">
+                <a href="https://zyyedi.github.io/csc412lab/about.php" class="nav-link">ABOUT</a>
+            </li>
+            <li class="nav-item">
+                <a href="https://zyyedi.github.io/csc412lab/ticket.php" class="nav-link">TICKETS</a>
+            </li>
+            <li class="nav-item">
+                <a href="https://zyyedi.github.io/csc412lab/login.php" class="nav-link">Log In</a>
+            </li>
+            <li class="nav-item">
+                <a href="https://zyyedi.github.io/csc412lab/signup.php" class="nav-link">Sign Up</a>
+            </li>
+        </ul>
+    </div>
+</nav>
